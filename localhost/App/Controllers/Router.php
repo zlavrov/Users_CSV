76a063af6f21edbf34665da03868883e023c93ee
zlavrov <zakhar.polosky@gmail.com>
2022-09-06 10:23:40 +0300

    Create Router.php
    Adding Router.php to the Controller, adding a controller to check the connection with the database and if everything is fine, then accept get or post parameters

---
+++ localhost/App/Controllers/Router.php
@@ -0,0 +1,51 @@
+<?php
+
+    namespace App\Controllers;
+
+    use App\Models\Clear;
+    use App\Models\Connection;
+    use App\Models\Insert;
+    use App\Models\Errorhandler;
+
+
+class Router {
+
+
+    /**
+     * If there is a connection to the database, then it further determines the method and directs it along the route
+     */
+
+    public static function start() {
+
+        if(self::connect()) {
+            if ($_SERVER['REQUEST_METHOD'] === "POST") {
+                $parametr = $_GET['parametr'];
+                if ($parametr == "Insert") {
+                    Insert::insert();
+                } else if ($parametr == "clear") {
+                    Clear::clear();
+                }
+            } else if ($_SERVER['REQUEST_METHOD'] === "GET") {
+                $parametr = $_GET['parametr'];
+                require_once "App/views/pages/" . $parametr . ".php";
+            }
+
+        } else {
+            Errorhandler::Errorhandler("Database error: " . $e->getMessage());
+        }
+    }
+
+    
+
+    /**
+     * Gets a response about the state of the connection to the database
+     */
+
+    public static function connect() {
+
+        return Connection::connection();
+
+    }
+}
+
+?>
