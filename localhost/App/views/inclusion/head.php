57e1351f8bb52cf1ac9c4fad4def6ef581f62568
zlavrov <zakhar.polosky@gmail.com>
2022-09-06 10:28:37 +0300

    Create head.php
    Adding head.php to display the head of the page, and include styles and scripts

---
+++ localhost/App/views/inclusion/head.php
@@ -0,0 +1,13 @@
+<!-- The head of the page where styles are included -->
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/App/views/components/styles/bootstrap.min.css">
+    <link rel="stylesheet" href="/App/views/components/styles/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <title>Apps</title>
+</head>
